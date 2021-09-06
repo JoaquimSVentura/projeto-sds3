8ff109a669ed6648e29c0c874448252d045b0c46
Joaquim <nowjoka28@gmail.com>
2021-09-06 17:24:11 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import Footer from "assets/components/Footer";
+import NavBar from "assets/components/Navbar";
+import DataTable from "assets/components/DataTable";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/assets/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{" "}
+          <a
+            href="https://www.linkedin.com/in/joaquim-ventura-9a1964181/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Joaquim Ventura
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
